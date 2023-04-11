f24e344ce7e2314f05f8fc6ab9486b2667ed1724
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2023-04-11 16:16:36 +0500

    add and subtract logic

--- app/src/main/java/com/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/basiccalculator/MainActivity.kt
@@ -9,49 +9,42 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.basiccalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnAd: Button = findViewById(R.id.btnAdd)
+        val btnSu: Button = findViewById(R.id.btnSub)
+        val edtN1: EditText = findViewById(R.id.edtNo1)
+        val edtN2: EditText = findViewById(R.id.edtNo2)
 
-     binding = ActivityMainBinding.inflate(layoutInflater)
-     setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        btnAd.setOnClickListener {
+            if (edtN1.text.toString() != "" && edtN2.text.toString() != "") {
+                val no1 = edtN1.text.toString().toInt()
+                val no2 = edtN2.text.toString().toInt()
+                val sum = no1 + no2
+                Toast.makeText(this, "The Sum is $sum", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Please Fill All the data", Toast.LENGTH_LONG).show()
+            }
         }
-    }
-override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        btnSu.setOnClickListener {
+            if (edtN1.text.toString() != "" && edtN2.text.toString() != "") {
+                val no1 = edtN1.text.toString().toInt()
+                val no2 = edtN2.text.toString().toInt()
+                val diff = no1 - no2
+                Toast.makeText(this, "The Difference is $diff", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Please Fill All the data", Toast.LENGTH_LONG).show()
+            }
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-    val navController = findNavController(R.id.nav_host_fragment_content_main)
-    return navController.navigateUp(appBarConfiguration)
-            || super.onSupportNavigateUp()
     }
 }
